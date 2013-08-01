2a48dbc7de9d5d3121ebeb79fa5b99be10416a04
Andy Pliszka <andy@isp44.com>
2013-08-01 01:25:46 -0400

    Moves iToDoTests to Tests directory

---
+++ Tests/iToDoTests.m
@@ -0,0 +1,32 @@
+//
+//  iToDoTests.m
+//  iToDoTests
+//
+//  Created by Andy Pliszka on 8/1/13.
+//  Copyright (c) 2013 Andy Pliszka. All rights reserved.
+//
+
+#import "iToDoTests.h"
+
+@implementation iToDoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertTrue(YES, @"YES should be TRUE");
+}
+
+@end
