fec0747e215836d0f9a5ecb24c632ed050f66b4c
Andy Pliszka <andy@isp44.com>
2013-08-02 01:40:50 -0400

    Adds examples of replacing methods

--- Specs/ExampleSpec.mm
+++ Specs/ExampleSpec.mm
@@ -1,10 +1,73 @@
+#import <objc/objc-runtime.h>
 using namespace Cedar::Matchers;
+
 using namespace Cedar::Doubles;
+
+@interface Person : NSObject
+
+- (void)sayHello;
+
+@end
+
+
+@implementation Person
+
+- (void)sayHello {
+	NSLog(@"hello");
+}
+
+- (void)skew:(int)n {
+	NSLog(@"skew %d", n);
+}
+
+@end
+
+@interface Book : NSObject
+
+- (void)foo;
+- (void)bar;
+
+@end
+
+@implementation Book
+
+- (void)foo;
+{
+    NSLog(@"foo");
+    [self bar];
+}
+
+- (void)bar;
+{
+    NSLog(@"bar");
+}
+
+- (void)baz;
+{
+    NSLog(@"baz");
+}
+
++ (BOOL) resolveInstanceMethod:(SEL)aSEL
+{
+    NSLog(@"resolvedInstanceMethod: %@", NSStringFromSelector(aSEL));
+    return [super resolveInstanceMethod:aSEL];
+}
+
+@end
+
+static IMP originalPaste = NULL;
+
+void myPaste(id rcv, SEL cmd)
+{
+    NSLog(@"myPaste");
+    // Your implementation here
+}
 
 SPEC_BEGIN(ExampleSpec)
 
 /* This is not an exhaustive list of usages.
    For more information, please visit https://github.com/pivotal/cedar */
+
 
 describe(@"Example specs on NSString", ^{
 
@@ -31,7 +94,64 @@ describe(@"Example specs on NSString", ^{
             [NSMutableString string] should be_instance_of([NSString class]).or_any_subclass();
         });
     });
+    
+    describe(@"Book", ^{
+        it(@"should return class name", ^{
+            NSLog(@"class name: %s", class_getName([Book class]));
+            NSLog(@"super class name: %s", class_getName(class_getSuperclass([Book class])));
+        });
+        
+        it(@"should replace one method with another method", ^{
+            Book *book = [[Book alloc] init];
+            Method bar = class_getInstanceMethod([book class], @selector(bar));
+            Method baz = class_getInstanceMethod([book class], @selector(baz));
+            
+            IMP imp = method_getImplementation(baz);
+            
+            [book bar];
+            method_setImplementation(bar, imp);
+            [book bar];
+            [book baz];
+        });
+        
+        it(@"should replace method with function", ^{
+            Book *book = [[Book alloc] init];
+            Book *book2 = [[Book alloc] init];
+            Method bar = class_getInstanceMethod([book class], @selector(bar));
+            
+            [book bar];
+            IMP old = method_setImplementation(bar, (IMP)myPaste);
+            [book bar];
+            [book baz];
+            method_setImplementation(bar, old);
+            [book2 bar];
+        });
+        
+        it(@"should replace method with block", ^{
+            Person *person = [[Person alloc] init];
+            [person sayHello];
+            
+            IMP a = imp_implementationWithBlock(^{
+                NSLog(@"hello again");
+            });
+            
+            class_replaceMethod([Person class], @selector(sayHello), a, NULL);
+            
+            [person sayHello];
+        });
+        
+        fit(@"should replace method with block with arguments", ^{
+            Person *person = [[Person alloc] init];
+            [person skew:1];
+            
+            IMP skewIMP = imp_implementationWithBlock(^(id _s, int k) { NSLog(@"block skew %d", k); });
+            
+            class_replaceMethod([Person class], @selector(skew:), skewIMP, "v@:i");
+            
+            [person skew:9];
+        });
+    });
+    
 });
 
 SPEC_END
-
